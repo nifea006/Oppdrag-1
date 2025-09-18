74834ec25912a88c0c17a7c7a05b6f04142d29a0
nifea006 <fedoseevnikita11110@gmail.com>
2025-09-18 12:04:31 +0200

    alder spørsmal update

--- fil-i-repo.py
+++ fil-i-repo.py
@@ -1,5 +1,5 @@
 print("Det er en fil i repository!")
-alder = input("How old are you?")
+alder = int(input("How old are you?"))
 if alder >= 18:
     print("Du må kjøpe en adult billett")
 elif alder <= 17:
